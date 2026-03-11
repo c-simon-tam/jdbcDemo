40716649a61648c978cba8247d0f66b7a5a5e0a6
stam <stam@cis.edu.hk>
2026-03-11 15:05:49 +0800

    can run a query and display the results

--- src/Main.java
+++ src/Main.java
@@ -1,30 +1,32 @@
 import java.sql.*;
 
-public class DBTest {
+public class Main {
+    private static void printResultSet(ResultSet rs) throws SQLException {
+        ResultSetMetaData metaData = rs.getMetaData();
+        int cols = metaData.getColumnCount();
+
+        for (int i=1; i <= cols; i++) {
+            System.out.print(metaData.getColumnLabel(i));
+            System.out.print("\t");
+        }
+        System.out.println();
+
+        while (rs.next()) {
+            for (int i=1; i <= cols; i++) {
+                System.out.print(rs.getString(i));
+                System.out.print("\t");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:sakila_master.db");
             Statement stmt = conn.createStatement();
-            String sql = "select title, description, release_year from film";
+            String sql = "select * from actor";
             ResultSet rs = stmt.executeQuery(sql);
-
-            ResultSetMetaData metaData = rs.getMetaData();
-            int cols = metaData.getColumnCount();
-
-            for (int i=1; i <= cols; i++) {
-                System.out.print(metaData.getColumnLabel(i));
-                System.out.print("\t");
-            }
-            System.out.println();
-
-            while (rs.next()) {
-                for (int i=1; i <= cols; i++) {
-                    System.out.print(rs.getString(i));
-                    System.out.print("\t");
-                }
-                System.out.println();
-            }
-
+            printResultSet(rs);
             conn.close();
         } catch (Exception e) {
             e.printStackTrace();
